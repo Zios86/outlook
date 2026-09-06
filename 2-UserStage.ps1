7b5b83eaac5e09a06dacc24654a05132f5f4b7c1
Zios86 <midnighthj@gmail.com>
2026-09-06 22:10:19 +0300

    Перевести пользовательский этап на V3

--- 2-UserStage.ps1
+++ 2-UserStage.ps1
@@ -8,7 +8,7 @@ param([Parameter(Mandatory)][string]$DestinationRoot)
 
 $ErrorActionPreference = 'Stop'
 $Sid = [Security.Principal.WindowsIdentity]::GetCurrent().User.Value
-$Root = Join-Path $env:ProgramData "OutlookPstMigrationSafeV2\$Sid"
+$Root = Join-Path $env:ProgramData "OutlookPstMigrationSafeV3\$Sid"
 $ResultFile = Join-Path $Root 'result.json'
 $LogFile = Join-Path $Root 'migration.log'
 $LockFile = Join-Path $Root 'running.lock'
